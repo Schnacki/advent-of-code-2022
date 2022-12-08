fb6530872e27dc2f18b67ee0b44547f3962c0c85
Kai Schnackenberg <kai.schnackenberg@rewe-digital.com>
2022-12-08 04:18:36 +0100

    Solution for Day 7

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,22 +1,28 @@
+import java.net.URL
+
 object App {
-    private fun readInputLines(f: String): List<String> = App::class.java.getResource(f)!!.readText().lines()
+
+    private const val GREEN_ARROW = "\u001B[32m✔\u001B[0m"
+
+    private fun readResourceLines(f: String): List<String> = (App::class.java.getResource(f) as URL).readText().lines()
 
     fun solveDay(day: Int, d: Day) {
-        val input = readInputLines("day$day.txt")
-        println("┣━━━━━━━━━━━━━━━━━━━━━━━━━━┫")
-        println("┃ Day %02d 1: %12s \u001B[32m✔\u001B[0m ┃".format(day, d.part1(input)))
-        println("┃ Day %02d 2: %12s \u001B[32m✔\u001B[0m ┃".format(day, d.part2(input)))
+        val input = readResourceLines("day$day.txt")
+        println("┣━━━━━━━━━━━━━━━━━━━━━━━━━┫")
+        println("┃ Day %02d 1: %11s %s ┃".format(day, d.part1(input), GREEN_ARROW))
+        println("┃ Day %02d 2: %11s %s ┃".format(day, d.part2(input), GREEN_ARROW))
     }
 }
 
 fun main() {
-    println("┏━━━━━━━━━━━━━━━━━━━━━━━━━━┓")
-    println("┃ AOC 2022                 ┃")
+    println("┏━━━━━━━━━━━━━━━━━━━━━━━━━┓")
+    println("┃   Advent of Code 2022   ┃")
     App.solveDay(1, Day1)
     App.solveDay(2, Day2)
     App.solveDay(3, Day3)
     App.solveDay(4, Day4)
     App.solveDay(5, Day5)
     App.solveDay(6, Day6)
-    println("┗━━━━━━━━━━━━━━━━━━━━━━━━━━┛")
+    App.solveDay(7, Day7)
+    println("┗━━━━━━━━━━━━━━━━━━━━━━━━━┛")
 }
---
+++ src/test/kotlin/Day7Test.kt
@@ -0,0 +1,41 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day7Test {
+
+    private val input = listOf(
+        "$ cd /",
+        "$ ls",
+        "dir a",
+        "14848514 b.txt",
+        "8504156 c.dat",
+        "dir d",
+        "$ cd a",
+        "$ ls",
+        "dir e",
+        "29116 f",
+        "2557 g",
+        "62596 h.lst",
+        "$ cd e",
+        "$ ls",
+        "584 i",
+        "$ cd ..",
+        "$ cd ..",
+        "$ cd d",
+        "$ ls",
+        "4060174 j",
+        "8033020 d.log",
+        "5626152 d.ext",
+        "7214296 k"
+    )
+
+    @Test
+    fun `Part 1 with example data`() {
+        assertEquals(95437, Day7.part1(input))
+    }
+
+    @Test
+    fun `Part 2 with example data`() {
+        assertEquals(24933642, Day7.part2(input))
+    }
+}
